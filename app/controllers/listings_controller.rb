63133d0283b5defe56b5c82a8c79f67213772c6a
Alan Fidelino <afidedev26@gmail.com>
2015-10-11 20:09:57 +1100

    Fixed design of listing pages

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,52 +1,50 @@
 class ListingsController < ApplicationController
+  def index
+  	@listings = Listing.all
+  end
 
+  def show
+  	@listing = Listing.find(params[:id])
+  end
 
-	def index
-		@listings = Listing.all
-	end
+  def new
+  	@listing = Listing.new
+  end
 
-	def show
-		@listing = Listing.find(params[:id])
-	end
+  def create
+  	@listing = Listing.new(listing_params)
+  	if @listing.save
+  		redirect_to @listing
+  		flash[:notice] = "New listing for #{@listing.name} has been created."
+  	else
+  		render "new"
+  	end
+  end
 
-	def new
-		@Listing = Listing.new
-	end
+  def edit
+  	@listing = Listing.find(params[:id])
+  end
 
-	def create
-		@Listing = Listing.new(listing_params)
-		if @Listing.save
-			redirect_to @listing
-			flash[:notice] = "New listing for #{@listing.name} has been created"
-		else
-			render "new"
-		end
-	end
+  def update
+  	@listing = Listing.find(params[:id])
+  	if @listing.update(listing_params)
+  		redirect_to @listing
+  		flash[:notice] = "#{@listing.name} has been updated."
+  	else
+  		render "edit"
+  	end
+  end
 
-	def edit
-		@listing = Listing.find(params[:id])
-	end
+  	def destroy
+  		@listing = Listing.find(params[:id])
+  		@listing.destroy
+  		flash[:danger] = "#{@listing.name} has been deleted from the database."
+  		redirect_to listings_path
+  	end
 
-	def update
-		if @listing.update(listing_params)
-			redirect_to @listing
-			flash[:notice] = "#{@listing.name} has been updated."
-		else
-			render "edit"
-		end
-	end
+  	private
 
-	def destroy
-		@listing = Listing.find(params[:id])
-		@listing.destroy
-		redirect_to root_url
-		flash[:notice] = "The listing for #{@listing.name} has been deleted."
-	end
-
-	private
-
-	def listing_params
-		params.require(:listing).permit(:name, :description, :price)
-	end
-
+  		def listing_params
+  			params.require(:listing).permit(:name, :description, :price)
+  		end
 end
